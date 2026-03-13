5b6b4a92690ff0fcadaf73b05ee8c204dd7c5628
Haziii1 <nguyenduchaikm1@gmail.com>
2026-03-13 19:47:04 +0700

    refactor: CourseResponse and Student Response

--- src/main/java/dto/StudentResponse.java
+++ src/main/java/dto/StudentResponse.java
@@ -1,26 +1,30 @@
 package dto;
 
+import java.util.*;
+
 public class StudentResponse {
     private int id;
     private String name;
     private String email;
     private String phone;
-
-    public StudentResponse(int id, String name, String email, String phone) {
+    private List<CourseResponse> courses;
+    public StudentResponse(int id, String name, String email, String phone , List<CourseResponse> courses) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.courses = courses;
     }
 
     public int getId()          { return id; }
     public String getName()     { return name; }
     public String getEmail()    { return email; }
     public String getPhone()    { return phone; }
-
+    public List<CourseResponse> getCourses() { return courses; }
     @Override
     public String toString() {
         return "StudentResponse [id=" + id + ", name=" + name
-                + ", email=" + email + ", phone=" + phone + "]";
+                + ", email=" + email + ", phone=" + phone
+                + ", courses=" + courses + "]";
     }
 }
